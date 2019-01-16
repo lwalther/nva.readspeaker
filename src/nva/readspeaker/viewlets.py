778df2c04373379460a1d99bc941f36a03f857b8
Lars Walther <lwalther@novareto.de>
2019-01-16 21:49:33 +0100

    Änderungen nach IE11-Bug

--- src/nva/readspeaker/viewlets.py
+++ src/nva/readspeaker/viewlets.py
@@ -1,19 +1,29 @@
+from datetime import datetime
 from zope.interface import Interface
 from uvc.api import api
 from plone import api as ploneapi
 from plone.app.layout.viewlets.interfaces import IAboveContentTitle
 from plone.app.layout.viewlets.interfaces import IAboveContent
+from plone.app.layout.viewlets.interfaces import IPortalHeader
 
 api.templatedir('templates')
 
 cid = '10429'
 readid = 'content'
 
+class ReadSpeakerScript(api.Viewlet):
+    api.context(Interface)
+    api.viewletmanager(IPortalHeader)
+    
+
 class ReadSpeakerViewlet(api.Viewlet):
     api.context(Interface)
     api.viewletmanager(IAboveContentTitle)
 
     def available(self):
+        #if hasattr(self.context, 'layout'):
+        #    if self.context.layout == 'folder_full_view':
+        #        return False
         return True
         allowedTypes = ['Document', 'News Item', 'Event', 'Folder',]
         if self.context.portal_type in allowedTypes:
@@ -22,4 +32,5 @@ class ReadSpeakerViewlet(api.Viewlet):
 
     def update(self):
         pageurl = self.context.absolute_url()
+        self.buttonid = 'readspeaker_button_%s' % datetime.now().strftime('%f')
         self.readurl = '//app-eu.readspeaker.com/cgi-bin/rsent?customerid=%s&amp;lang=de_de&amp;readid=%s&amp;url=%s' % (cid, readid, pageurl) 
